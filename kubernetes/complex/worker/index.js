4330173fa00f9269d46b1c3f80e0e418f9a52c27
mwajdzik <amw061@gmail.com>
2018-12-16 10:32:05 +0100

    Create a copy of the complex project for k8

---
+++ kubernetes/complex/worker/index.js
@@ -0,0 +1,28 @@
+const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const subscription = redisClient.duplicate();
+
+function fib(index) {
+    if (index < 2) {
+        return 1;
+    } else {
+        return fib(index - 1) + fib(index - 2);
+    }
+}
+
+subscription.on('message', (channel, message) => {
+    const value = fib(parseInt(message));
+    redisClient.hset('values', message, value);
+    console.info('Calculated fib value for ' + message + ' to be ' + value);
+});
+
+subscription.subscribe('insert');
+
+console.info("Worker started!");
