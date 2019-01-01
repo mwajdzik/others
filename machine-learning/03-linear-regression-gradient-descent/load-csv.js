e6deeb9684c84d88f25e788d45b4e18fc8827a0d
mwajdzik <amw061@gmail.com>
2019-01-01 21:02:28 +0100

    Refactor LoadCSV

--- machine-learning/03-linear-regression-gradient-descent/load-csv.js
+++ machine-learning/03-linear-regression-gradient-descent/load-csv.js
@@ -3,67 +3,65 @@ const _ = require('lodash');
 const shuffleSeed = require('shuffle-seed');
 
 function extractColumns(data, columnNames) {
-  const headers = _.first(data);
+    const headers = _.first(data);
+    const indexes = _.map(columnNames, column => headers.indexOf(column));
 
-  const indexes = _.map(columnNames, column => headers.indexOf(column));
-  const extracted = _.map(data, row => _.pullAt(row, indexes));
-
-  return extracted;
+    return _.map(data, row => _.pullAt(row, indexes));
 }
 
-module.exports = function loadCSV(
-  filename,
-  {
+module.exports = function loadCSV(filename, {
     dataColumns = [],
     labelColumns = [],
     converters = {},
     shuffle = false,
     splitTest = false
-  }
-) {
-  let data = fs.readFileSync(filename, { encoding: 'utf-8' });
-  data = _.map(data.split('\n'), d => d.split(','));
-  data = _.dropRightWhile(data, val => _.isEqual(val, ['']));
-  const headers = _.first(data);
+}) {
+    let data = fs.readFileSync(filename, { encoding: 'utf-8' });
+    data = _.map(data.split('\n'), d => d.split(','));
+    data = _.dropRightWhile(data, val => _.isEqual(val, ['']));
 
-  data = _.map(data, (row, index) => {
-    if (index === 0) {
-      return row;
-    }
-    return _.map(row, (element, index) => {
-      if (converters[headers[index]]) {
-        const converted = converters[headers[index]](element);
-        return _.isNaN(converted) ? element : converted;
-      }
+    const headers = _.first(data);
 
-      const result = parseFloat(element.replace('"', ''));
-      return _.isNaN(result) ? element : result;
+    data = _.map(data, (row, index) => {
+        if (index === 0) {
+            return row;
+        }
+
+        return _.map(row, (element, index) => {
+            const converter = converters[headers[index]];
+            
+            if (converter) {
+                const converted = converter(element);
+                return _.isNaN(converted) ? element : converted;
+            }
+
+            const result = parseFloat(element.replace('"', ''));
+            return _.isNaN(result) ? element : result;
+        });
     });
-  });
 
-  let labels = extractColumns(data, labelColumns);
-  data = extractColumns(data, dataColumns);
+    let labels = extractColumns(data, labelColumns);
+    data = extractColumns(data, dataColumns);
 
-  data.shift();
-  labels.shift();
+    // remove headers
+    data.shift();
+    labels.shift();
 
-  if (shuffle) {
-    data = shuffleSeed.shuffle(data, 'phrase');
-    labels = shuffleSeed.shuffle(labels, 'phrase');
-  }
+    if (shuffle) {
+        data = shuffleSeed.shuffle(data, 'phrase');
+        labels = shuffleSeed.shuffle(labels, 'phrase');
+    }
 
-  if (splitTest) {
-    const trainSize = _.isNumber(splitTest)
-      ? splitTest
-      : Math.floor(data.length / 2);
+    if (splitTest) {
+        const trainSize = _.isNumber(splitTest) ? splitTest : Math.floor(data.length / 2);
 
-    return {
-      features: data.slice(trainSize),
-      labels: labels.slice(trainSize),
-      testFeatures: data.slice(0, trainSize),
-      testLabels: labels.slice(0, trainSize)
-    };
-  } else {
-    return { features: data, labels };
-  }
+        return {
+            features: data.slice(trainSize),
+            labels: labels.slice(trainSize),
+            testFeatures: data.slice(0, trainSize),
+            testLabels: labels.slice(0, trainSize)
+        };
+    } else {
+        return { features: data, labels };
+    }
 };
