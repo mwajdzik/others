3c74b20707311471a2f3e978bd362014f7a3c606
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2019-12-30 17:17:11 +0100

    Docker

--- docker/02-simple-visits/index.js
+++ docker/02-simple-visits/index.js
@@ -4,18 +4,20 @@ const process = require("process");
 
 const app = express();
 
-// redis-service - from docker-compose.yml
+// "redis-service" - from docker-compose.yml
 const client = redis.createClient({
     host: 'redis-service',
     port: 6379
 });
 
-client.set('visits', 1);
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
+        visits = parseInt(visits) + 1;
+
         res.send('Number of visits is ' + visits);
-        client.set('visits', parseInt(visits) + 1);
+        client.set('visits', visits);
 
         if (visits % 5 === 0) {
             console.log("Restarting...");
